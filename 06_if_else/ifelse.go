319b7d49c3ca4d72cd4ca66e4a067b85e8de1fe2
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-12-04 12:08:17 +0545

    add if-else demonstrations in ifelse.go

---
+++ 06_if_else/ifelse.go
@@ -0,0 +1,41 @@
+package main
+
+func main() {
+	// age := 16
+
+	// if age >= 18 {
+	// 	fmt.Println("Person is an Adult.")
+	// }  else {
+	// 	fmt.Println("Person is not an Adult.")
+	// }
+
+	// age := 2
+
+	// if age >= 18 {
+	// 	fmt.Println("Person is an Adult.")
+	// } else if age >= 12 {
+	// 	fmt.Println("Person is not an Adult.")
+	// } else if age >= 5 {
+	// 	fmt.Println("Person in Child")
+	// } else {
+	// 	fmt.Println("You just Baby👶")
+	// }
+
+	// var role = "admin"
+	// var hasPermissions = true
+
+	// if role == "admin" && hasPermissions {
+	// 	fmt.Println("You are Authorized user✅")
+	// } else {
+	// 	fmt.Println("You are Unauthorized")
+	// }
+
+	// WE can declare a variable inside if construct
+	// if age := 15; age > 18 {
+	// 	fmt.Println("Preson is Adult", age)
+	// } else if age >= 12 {
+	// 	fmt.Println("Person is teenager", age)
+	// }
+
+	// go does not have ternary, you will have to use normal if else
+}
