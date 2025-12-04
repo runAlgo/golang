1b47a711776646d1a01cbef18d9d090bb2ab6050
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-12-04 11:18:42 +0545

    add variables demonstration in variables.go

---
+++ 03_variables/variables.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	// string
+	// var name string = "golang"
+
+	// infer
+	// var name = "golang"
+	// var isAdult = true
+
+	// var age int = 30
+
+	// shorthand syntax
+
+	// name := "golang"
+
+	// var name string
+	// name = "golang"
+
+	// var price float32 = 50.5
+	// var price = 50.5
+	price := 50.5
+	fmt.Println(price)
+}
