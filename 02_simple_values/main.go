1409ff64fbed9bb5581cfaee05ef0528a7c1588e
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-12-04 11:08:02 +0545

    add simple values demonstration in main.go

---
+++ 02_simple_values/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple Values
+	// Int
+	fmt.Println(1 + 1)
+	// string
+	fmt.Println("Hello golang")
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+	fmt.Println(10.5)
+	fmt.Println(7.0 / 3.0)
+}
