983af485d6cba6b6ced38c5562ce6ecd12ba826c
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-12-04 15:55:37 +0545

    add array demonstration in array.go

---
+++ 08_array/array.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+// Array -> numbered sequence of specific length
+func main() {
+	// int -> 0,  string -> "", bool -> false
+
+
+	// var nums [4]bool
+
+	// for i := 0; i < len(nums); i++ {
+	// 	if i%2 == 0 {
+	// 		nums[i] = true
+	// 	} else {
+	// 		nums[i] = false
+	// 	}
+	// }
+
+	// var users [4]string
+
+	// for i := 0; i < len(users); i++ {
+	// 	users[i] = fmt.Sprintf("User%d",i)
+	// }
+
+	// fmt.Println(users[3])
+
+
+
+
+	// To declare it in single line
+
+	// nums := [3]int{1,2,3}
+	// fmt.Println((nums))
+
+
+	// 2d arrays
+	// nums := [2][2]int{{3, 4}, {5, 6}}
+	// fmt.Println(nums)
+
+
+	// Benifits of using Array
+	// - Fixed size that is predictable
+	// - Memory optimization
+	// - Constant time access
+}
