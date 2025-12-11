30938fed543927803e2aee0bd0371626ba23746c
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-12-11 16:19:11 +0545

    add pointers.go to demonstrate passing by reference in Go

---
+++ 15_pointers/pointers.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// by value
+// func changeNum(num int) {
+// 	num = 5
+// 	fmt.Println("In changeNum", num)
+// }
+
+// by refrance
+func changeNum(num *int) {
+	*num = 5
+	fmt.Println("In changeNum", *num)
+}
+
+func main() {
+	num := 1
+	// changeNum(num)
+	changeNum(&num)
+
+	fmt.Println("Memory address", &num)
+	fmt.Println("After changeNum in main", num)
+}
