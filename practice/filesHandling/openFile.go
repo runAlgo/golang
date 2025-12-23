5b6a50c4717ff7332a0f1d068201e63df81e6be7
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-12-23 16:59:48 +0545

    feat: add readLinebyLine.go for efficient buffered reading of test.txt

--- practice/filesHandling/openFile.go
+++ practice/filesHandling/openFile.go
@@ -1,14 +1,9 @@
 package main
 
-import (
-	"log"
-	"os"
-)
-
-func main() {
-	file, err := os.Open("test.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-}
+// func main() {
+// 	file, err := os.Open("test.txt")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer file.Close()
+// }
