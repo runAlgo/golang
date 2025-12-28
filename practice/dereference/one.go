e8a58c7ebf397bcd976f0fc40e832e04b99d7cc6
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-12-28 14:26:31 +0545

    Basic Dereferencing

---
+++ practice/dereference/one.go
@@ -0,0 +1,18 @@
+package main
+
+// Basic Derefrencing
+// func main() {
+// 	secretNumber := 42
+
+// 	// 1. Reference: Get the address (&)
+// 	ptr := &secretNumber
+
+// 	fmt.Println("Address:", ptr)
+
+// 	// 2. Dereference: Get the value (*)
+// 	fmt.Println("Value:", *ptr)
+
+// 	// 3. Mutation via Dereferance
+// 	*ptr = 100 // Changes the value at that address
+// 	fmt.Println("New Value:", secretNumber)
+// }
