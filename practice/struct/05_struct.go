fd1e0c58a9c6c6d7762d305d30c2d6975c5f3404
runAlgo <150834861+runAlgo@users.noreply.github.com>
2026-01-07 13:52:49 +0545

    feat: add User struct and methods example in main.go; comment out previous struct examples

--- practice/struct/05_struct.go
+++ practice/struct/05_struct.go
@@ -1,49 +1,49 @@
 package main
 
-import (
-	"time"
-)
+// import (
+// 	"time"
+// )
 
-// 2. The Data Mode Struct (Database/API Entity)
-// This is the most common type of struct, used to represent data
-// entities flowing in your system, often mapping to a database table
-// or a JOSN payload. It relies heavily on struct tags.
+// // 2. The Data Mode Struct (Database/API Entity)
+// // This is the most common type of struct, used to represent data
+// // entities flowing in your system, often mapping to a database table
+// // or a JOSN payload. It relies heavily on struct tags.
 
-// Confidence Builder: This pattern shows you how to model real-world
-// objects while simultaneously preparing them for database and API
-// interaction.
+// // Confidence Builder: This pattern shows you how to model real-world
+// // objects while simultaneously preparing them for database and API
+// // interaction.
 
-type User struct {
-	ID        int    `json:"id,omitempty" db:"user_id"`
-	FirstName string `json:"first_name" db:"first_name"`
-	LastName  string `json:"last_name" db:"last_name"`
-	Email     string `json:"email" db:"email"`
-	IsActive  bool   `json:"is_active" db:"is_active"`
+// type User struct {
+// 	ID        int    `json:"id,omitempty" db:"user_id"`
+// 	FirstName string `json:"first_name" db:"first_name"`
+// 	LastName  string `json:"last_name" db:"last_name"`
+// 	Email     string `json:"email" db:"email"`
+// 	IsActive  bool   `json:"is_active" db:"is_active"`
 
-	// Unexported (private) field for security/business logic
-	passwordHash string
+// 	// Unexported (private) field for security/business logic
+// 	passwordHash string
 
-	// time.Time is a common type for timestamps
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-}
-
-// FullName is a simple method on the User struct.
-
-func (u *User) FullName() string {
-	return u.FirstName + " " + u.LastName
-}
-
-// func main() {
-// 	newUser := User{
-// 		passwordHash: "sfdsbfdsof",
-// 		ID:           1,
-// 		FirstName:    "Kalu",
-// 		LastName:     "Don",
-// 		Email:        "kalu@gmail.com",
-// 		IsActive:     true,
-// 		CreatedAt:    time.Now(),
-// 	}
-
-// 	res := newUser.FullName()
-// 	fmt.Println(res)
+// 	// time.Time is a common type for timestamps
+// 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 // }
+
+// // FullName is a simple method on the User struct.
+
+// func (u *User) FullName() string {
+// 	return u.FirstName + " " + u.LastName
+// }
+
+// // func main() {
+// // 	newUser := User{
+// // 		passwordHash: "sfdsbfdsof",
+// // 		ID:           1,
+// // 		FirstName:    "Kalu",
+// // 		LastName:     "Don",
+// // 		Email:        "kalu@gmail.com",
+// // 		IsActive:     true,
+// // 		CreatedAt:    time.Now(),
+// // 	}
+
+// // 	res := newUser.FullName()
+// // 	fmt.Println(res)
+// // }
--- practice/struct/08_whereToUse.go
+++ practice/struct/08_whereToUse.go
@@ -1,46 +1,46 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-// In Go, the interface ensures that your code remains as flexible as the wall outlet.
+// // In Go, the interface ensures that your code remains as flexible as the wall outlet.
 
-// 1. The Interface (The Contract)
-type Speaker interface {
-	Speak() string
-}
+// // 1. The Interface (The Contract)
+// type Speaker interface {
+// 	Speak() string
+// }
 
-// 2. A Struct Implementation (A concrete object)
-type Dog struct {
-	Name string
-}
+// // 2. A Struct Implementation (A concrete object)
+// type Dog struct {
+// 	Name string
+// }
 
-// Dog implements the Speaker interface because it has Speak() method
-func (d Dog) Speak() string {
-	return "Woof!"
-}
+// // Dog implements the Speaker interface because it has Speak() method
+// func (d Dog) Speak() string {
+// 	return "Woof!"
+// }
 
-// 3. A Different Struct Implementaion
-type Cat struct {
-	Lives int
-}
+// // 3. A Different Struct Implementaion
+// type Cat struct {
+// 	Lives int
+// }
 
-// Cat also implements the Speaker interface
-func (c Cat) Speak() string {
-	return "Meow."
-}
+// // Cat also implements the Speaker interface
+// func (c Cat) Speak() string {
+// 	return "Meow."
+// }
 
-// 4. Client Code uses the Interface
-func Introduce(s Speaker) {
-	// We don't care if 's' is a Dog or a Cat, only that it can Speak()
-	fmt.Printf("I say: %s\n", s.Speak())
-}
+// // 4. Client Code uses the Interface
+// func Introduce(s Speaker) {
+// 	// We don't care if 's' is a Dog or a Cat, only that it can Speak()
+// 	fmt.Printf("I say: %s\n", s.Speak())
+// }
 
 
-func main() {
-	// Usage:
-	dog := Dog{Name: "Buddy"}
-	cat := Cat{Lives: 9}
+// func main() {
+// 	// Usage:
+// 	dog := Dog{Name: "Buddy"}
+// 	cat := Cat{Lives: 9}
 
-	Introduce(dog) // Output: I say: Woof!
-	Introduce(cat) // Output: I say: Meow!
-}
+// 	Introduce(dog) // Output: I say: Woof!
+// 	Introduce(cat) // Output: I say: Meow!
+// }
